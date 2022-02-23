a2906a7dd2b05603d8dab7d9f7c55706f7ba8c5e
geena-keem <geena.gamm@gmail.com>
2022-02-23 23:03:34 +0900

    classes on typescript

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
   // #1
   const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!`
+    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
   };
 
   console.log(sayHi("gee-na", 30, "female"));
 
 
-  // #2 interfaces
+
+  // #2 interface
   interface Human {
     name: string,
     age: number,
@@ -20,10 +21,32 @@
   }
 
   const sayHi2 = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!2`
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!2`;
   }
 
-  console.log(sayHi2(person))
+  console.log(sayHi2(person));
 
+
+  
+  // #3 class
+  class Human2 {
+    public name: string;
+    public age: number;
+    public gender: string;
+
+    constructor(name: string, age: number, gender: string) {
+      this.name = name;
+      this.age = age;
+      this.gender = gender;
+    }
+  }
+
+  const geena = new Human2("gee-----na", 30, "female");
+
+  const sayHi3 = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!3`;
+  }
+
+  console.log(sayHi3(geena));
 
   export {};
